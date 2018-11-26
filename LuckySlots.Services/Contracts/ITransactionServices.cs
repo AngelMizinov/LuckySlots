6139e8b78079844da7225ff87deecf8e7a8847c4
Robert Aleksovski <robert.a@msn.com>
2018-11-26 15:28:10 +0200

    Created transaction services and added test projects.

---
+++ LuckySlots.Services/Contracts/ITransactionServices.cs
@@ -0,0 +1,17 @@
+﻿namespace LuckySlots.Services.Contracts
+{
+    using LuckySlots.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ITransactionServices
+    {
+
+        Task<IEnumerable<Transaction>> GetAllAsync();
+
+        Task<IEnumerable<Transaction>> GetAllByIdAsync(string id);
+
+        Task<Transaction> CreateAsync(string userId, string type, decimal amount, string description);
+    }
+}
---
+++ LuckySlots.Services/Transactions/TransactionServices.cs
@@ -0,0 +1,60 @@
+﻿namespace LuckySlots.Services.Transactions
+{
+    using LuckySlots.Data;
+    using LuckySlots.Data.Models;
+    using LuckySlots.Services.Abstract;
+    using LuckySlots.Services.Contracts;
+    using Microsoft.AspNetCore.Identity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class TransactionServices : BaseService, ITransactionServices
+    {
+        private readonly UserManager<User> userManager;
+
+        public TransactionServices(
+            LuckySlotsDbContext context,
+            UserManager<User> userManager)
+            : base(context)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<Transaction> CreateAsync(string userId, string type, decimal amount, string description)
+        {
+            var user = await this.userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var transaction = new Transaction
+            {
+                Date = DateTime.Now,
+                User = user,
+                Type = type,
+                Amount = amount,
+                Description = description
+            };
+
+            await this.Context.Transactions.AddAsync(transaction);
+
+            await this.Context.SaveChangesAsync();
+
+            return transaction;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetAllAsync()
+            => await Task.Run(() => this.Context.Transactions);
+
+
+        public async Task<IEnumerable<Transaction>> GetAllByIdAsync(string id)
+            => await Task.Run(() => this.Context
+                .Transactions
+                .Where(t => t.UserId == id)
+                .ToList());
+    }
+}
