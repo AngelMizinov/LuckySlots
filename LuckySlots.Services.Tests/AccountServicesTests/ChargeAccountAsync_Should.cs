c5d944e87b9c78005f7835df47998c7d106045bb
Angel Mizinov <angelmizinov@abv.bg>
2018-11-26 15:39:59 +0200

    Add NuGet packages for UnitTests

---
+++ LuckySlots.Services.Tests/AccountServicesTests/ChargeAccountAsync_Should.cs
@@ -0,0 +1,20 @@
+﻿using LuckySlots.Services.Account;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckySlots.Services.Tests.AccountServicesTests
+{
+    [TestClass]
+    public class ChargeAccountAsync_Should
+    {
+        [TestMethod]
+        public void ReturnNewBalance_When_AccountIsCharged()
+        {
+        //    var mockContext = new Mock<>
+
+        //    var accountService = new AccountService();
+        }
+    }
+}
